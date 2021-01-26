4f1d6fd2c7bec94a56097592839df3bdc439812b
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-01-26 19:12:43 -0300

    Remove import; Exclui arquivos de test default.

--- app/Http/Controllers/Api/V1/Admin/GeneralClassificationApiController.php
+++ app/Http/Controllers/Api/V1/Admin/GeneralClassificationApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\V1\Admin;
 use App\Models\Racing;
 use Illuminate\Http\Request;
 use App\Models\RunnersResult;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\GeneralClassification;
 use Symfony\Component\HttpFoundation\Response;
